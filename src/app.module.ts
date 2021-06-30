c70134626a7e68d0c3711af65b94f69fb368e48d
lukassousaf <lukasdsfigueiredo@gmail.com>
2021-06-30 18:34:04 -0300

    Users

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SchedulesModule } from './schedules/schedules.module';
 import { MongooseModule } from '@nestjs/mongoose'
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://root:root@cluster0.p4h4m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'), SchedulesModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://root:root@cluster0.p4h4m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'), SchedulesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
